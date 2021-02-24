b833127b5cdc8485c1f851999bf5e401320cae1a
Yun Zhang <yzhang175@hawk.iit.edu>
2021-02-24 04:30:25 -0500

    send a JSON response

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Travelbriefing', greeting: 'Hello!' });
+});
+router.get('/index.json', function(req, res, next) {
+  res.json({ title: 'Travelbriefing', greeting: 'Hello!' });
 });
 
 module.exports = router;
