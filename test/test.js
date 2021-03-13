fb528c3829111165afb69aad11ce0450995766ad
Haoli Yang <hyang77@hawk.iit.edu>
2021-03-13 12:03:23 -0600

    Create testing file and fix errors caught from linter in test.js file

---
+++ test/test.js
@@ -0,0 +1,9 @@
+"use strict";
+/* global describe, it */
+const assert = require("assert").strict;
+// Add testing codes here
+describe("the truth", function () {
+  it("should be true", function () {
+    assert("true");
+  });
+});
