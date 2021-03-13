cef0ad1ac1c1ad643dca58cdadd1a6948c1ba4c8
Haoli Yang <hyang77@hawk.iit.edu>
2021-03-13 16:25:56 -0600

    Remove testing codes

--- test/test.js
+++ test/test.js
@@ -1,9 +1,3 @@
 "use strict";
-/* global describe, it */
 const assert = require("assert").strict;
 // Add testing codes here
-describe("the truth", function () {
-  it("should be true", function () {
-    assert("true");
-  });
-});
