abb549e23a34441519ac63768cbe1c16f8fa9fc7
Haoli Yang <hyang77@hawk.iit.edu>
2021-04-07 14:27:12 -0500

    Load environment variables to fix db connection issue

--- app.js
+++ app.js
@@ -1,100 +1,108 @@
-'use strict';
+"use strict";
 /** Require the Http-errors to create http errors for express
  * {@link https://www.npmjs.com/package/http-errors/}
     @constant
     @type {object}
  */
-var createError = require('http-errors');
+var createError = require("http-errors");
 /** Require express framework
  * {@link https://expressjs.com/}
     @constant
     @type {object}
  */
-var express = require('express');
+var express = require("express");
 /** Require path
     @constant
     @type {object}
  */
-var path = require('path');
+var path = require("path");
 /** Require cookie-parser
 *{@link https://www.npmjs.com/package/cookie-parser/}
     @constant
     @type {object}
  */
-var cookieParser = require('cookie-parser');
+var cookieParser = require("cookie-parser");
 /** Require body-parser
     @constant
     @type {object}
  */
-var bodyParser = require('body-parser');
+var bodyParser = require("body-parser");
 /** Require http request logger middleware morgan
 *{@link https://www.npmjs.com/package/morgan/}
     @constant
     @type {object}
  */
-var logger = require('morgan');
+var logger = require("morgan");
 
 /** Require mongoose
     @constant
     @type {object}
  */
- var mongoose = require('mongoose');
+var mongoose = require("mongoose");
+
+/** Require dotenv
+   {@link https://www.npmjs.com/package/dotenv}
+   @constant
+   @type {object}
+*/
+require("dotenv").config();
 
 /** Require the index route
     @constant
     @type {object}
  */
-var indexRouter = require('./routes/index');
+var indexRouter = require("./routes/index");
 /** Require the users route
     @constant
     @type {object}
  */
-var usersRouter = require('./routes/users');
+var usersRouter = require("./routes/users");
 /** Require the country route
     @constant
     @type {object}
  */
-var countryRouter = require('./routes/country');
+var countryRouter = require("./routes/country");
 
 var app = express();
 
 //establish mongosb connection
-mongoose.connect('mongodb+srv://jhuang:' + process.env.MONGO_ATLAS_PW +
-"@cluster0.4hjfq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" ,
-{
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true
-});
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PW}@cluster0.4hjfq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  }
+);
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //routes to handle requests
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/country', countryRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/country", countryRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res) {
+app.use(function (err, req, res) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
