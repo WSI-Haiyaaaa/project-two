bae27960ac540439c0bb270a41b8610243818ddc
Haoli Yang <54561518+hyang77@users.noreply.github.com>
2021-04-07 12:03:02 -0500

    Convert html to pug (#16)
    * Create pug files from html
    
    * Add pug routing
    
    * Include header.pug into other pug files
    
    * Include navigation.pug into other pug files
    
    * Fix routing path
    
    * Dynamically render country pages
    
    * Include footer.pug into other pug files
    
    * Remove html files

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,42 @@
 "use strict";
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index',
-    { title: 'Travelbriefing', greeting: 'Hello!' }
-  );
+router.get("/", function (req, res) {
+  res.render("index.pug");
+});
+/* GET feedback page. */
+router.get("/feedback", function (req, res) {
+  res.render("feedback");
+});
+/* GET china page. */
+router.get("/china", function (req, res) {
+  res.render("country", { countryName: "China" });
+});
+/* GET japan page. */
+router.get("/japan", function (req, res) {
+  res.render("country", { countryName: "Japan" });
+});
+/* GET malaysia page. */
+router.get("/malaysia", function (req, res) {
+  res.render("country", { countryName: "Malaysia" });
+});
+/* GET singapore page. */
+router.get("/singapore", function (req, res) {
+  res.render("country", { countryName: "Singapore" });
+});
+/* GET south korea page. */
+router.get("/south-korea", function (req, res) {
+  res.render("country", { countryName: "South Korea" });
+});
+/* GET thailand page. */
+router.get("/thailand", function (req, res) {
+  res.render("country", { countryName: "Thailand" });
 });
 
-router.get('/index.json', function(req, res) {
-  res.json({ title: 'Travelbriefing', greeting: 'Hello!' });
+router.get("/index.json", function (req, res) {
+  res.json({ title: "Travelbriefing", greeting: "Hello!" });
 });
 
 module.exports = router;
