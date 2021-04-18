6f5f0ffd83ed9a40b3eed413b763a58ed6da7208
Haoli Yang <hyang77@hawk.iit.edu>
2021-04-17 19:31:51 -0500

    Display country and culture data from DB

--- routes/china.js
+++ routes/china.js
@@ -6,6 +6,11 @@
  */
 var express = require("express");
 var router = express.Router();
+/** Require utility functions
+    @constant
+    @type {object}
+ */
+const utils = require("../lib/utils");
 /** Require Country scheme
     @constant
     @type {object}
@@ -17,77 +22,28 @@ const Country = require("../models/country_schema");
  */
 const Culture = require("../models/dos_and_donts_schema");
 
-async function getCoutryRecord() {
-  return Country.find({ "names.name": "China" })
-    .select("-__v")
-    .exec()
-    .then((record) => {
-      if (!record) {
-        // If not, set status 404 Not Found
-        res.status(404).json({
-          error: "Country not found",
-        });
-      } else {
-        // If exists, set status 200 OK and render the page with the object
-        return record;
-      }
-    })
-    .catch((err) => {
-      // Set status of 500 Internal Server Error if an error is catch
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-}
-
-async function getCultureRecord() {
-  return Culture.find({ countryName: "china" })
-    .select("-__v")
-    .exec()
-    .then((record) => {
-      // Check if the record id exists
-      if (!record) {
-        // If not, set status 404 Not Found
-        res.status(404).json({
-          error: "Country not found",
-        });
-      } else {
-        // If exists, set status 200 OK and render the page with the object
-        return record;
-      }
-    })
-    .catch((err) => {
-      // Set status of 500 Internal Server Error if an error is catch
-      console.log(err);
-      res.status(500).json({
-        error: err,
-      });
-    });
-}
-
 /* GET china page. */
 router.get("/", async function (req, res) {
-  try {
-    const countryRecord = await getCoutryRecord();
-    const cultureRecord = await getCultureRecord();
-    if (!countryRecord) {
-      // If not, set status 404 Not Found
-      res.status(404).json({
-        error: "Country not found",
-      });
-    } else if (!cultureRecord) {
-      res.status(404).json({
-        error: "Culture not found",
-      });
-    } else {
-      console.log(cultureRecord);
-      res
-        .status(200)
-        .render("country", { country: countryRecord, culture: cultureRecord });
-    }
-  } catch (e) {
-    console.log(e);
+  const countryRecord = await utils.getRecordFromDB(Country, {
+    "names.name": "China",
+  });
+  const cultureRecord = await utils.getRecordFromDB(Culture, {
+    countryName: "china",
+  });
+  if (!countryRecord) {
+    // If not, set status 404 Not Found
+    res.status(404).json({
+      error: "Country not found",
+    });
+  } else if (!cultureRecord) {
+    // If not, set status 404 Not Found
+    res.status(404).json({
+      error: "Culture not found",
+    });
   }
+  // If exists, set status 200 OK and render the page with the object
+  res
+    .status(200)
+    .render("country", { country: countryRecord, culture: cultureRecord });
 });
 module.exports = router;
--- routes/malaysia.js
+++ routes/malaysia.js
@@ -6,32 +6,45 @@
  */
 var express = require("express");
 var router = express.Router();
-/** Require Country scheme
+/** Require utility functions
     @constant
     @type {object}
  */
-const Country = require('../models/country_schema');
+const utils = require("../lib/utils");
+/** Require Country scheme
+    @constant
+    @type {object}
+  */
+    const Country = require("../models/country_schema");
+/** Require Culture scheme
+    @constant
+    @type {object}
+  */
+const Culture = require("../models/dos_and_donts_schema");
 
-/* GET china page. */
-router.get("/", function (req, res) {
-  Country.find({"names.name": "Malaysia"})
-    .select('-__v')
-    .exec()
-    .then((record) => { // Check if the record id exists
-      if(!record) { // If not, set status 404 Not Found
-        res.status(404).json({
-          error: "Country not found"
-        });
-      } else { // If exists, set status 200 OK and render the page with the object
-        res.status(200).render("country", { country: record });
-      }
-    })
-    .catch((err) => { // Set status of 500 Internal Server Error if an error is catch
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
+/* GET malaysia page. */
+router.get("/", async function (req, res) {
+  const countryRecord = await utils.getRecordFromDB(Country, {
+    "names.name": "Malaysia",
+  });
+  const cultureRecord = await utils.getRecordFromDB(Culture, {
+    countryName: "malaysia",
+  });
+  if (!countryRecord) {
+    // If not, set status 404 Not Found
+    res.status(404).json({
+      error: "Country not found",
     });
+  } else if (!cultureRecord) {
+    // If not, set status 404 Not Found
+    res.status(404).json({
+      error: "Culture not found",
+    });
+  }
+  // If exists, set status 200 OK and render the page with the object
+  res
+    .status(200)
+    .render("country", { country: countryRecord, culture: cultureRecord });
 });
 
 module.exports = router;
