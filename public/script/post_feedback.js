dbccd6889a41992dfde353b89b6990da54e8130d
jhuang60 <jhuang60@hawk.iit.edu>
2021-04-19 15:17:56 -0500

    created js cript and used ajax to handle user input

---
+++ public/script/post_feedback.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+/** onlick to submit feedback form*/
+  $("#submitFeedback").on("click", function(e){
+    e.preventDefault();
+    submitFeedbackForm();
+  })
+});
+
+/** function to handle user feedback input*/
+function submitFeedbackForm(){
+/** user input saved in variables */
+  let subject = $('#subject').val();
+  let email = $('#email').val();
+  let feedback = $('#feedback-text').val();
+
+/** ajax post request for feedback */
+  $.ajax("http://localhost:3000/feedback",{
+    type : "POST",
+    data : {
+      subject : subject,
+      email : email,
+      feedback : feedback
+    },
+    success : result => {
+      console.log("success ", result);
+      alert("Your feedback is successfully submitted. Thank you for your feedback")
+    },
+    error : error => {
+      console.log("error", error);
+      alert("Failed to submit feedback. Please try again.")
+    }
+  });
+}
