c9b4df595e73ddc03315b780c2a4e2402d0076f9
Haoli Yang <hyang77@hawk.iit.edu>
2021-05-08 13:49:32 -0500

    Replace routes with single and dynamic route

--- routes/country.js
+++ routes/country.js
@@ -1,4 +1,5 @@
 "use strict";
+const { request } = require("express");
 /** Require express framework
  * {@link https://expressjs.com/}
     @constant
@@ -14,31 +15,32 @@ const utils = require("../lib/utils");
 /** Require Country scheme
     @constant
     @type {object}
-  */  
+ */
 const Country = require("../models/country_schema");
 /** Require Culture scheme
     @constant
     @type {object}
-  */
+ */
 const Culture = require("../models/dos_and_donts_schema");
 
-/* GET singapore page. */
-router.get("/", async function (req, res) {
+/* GET china page. */
+router.get("/:country", async function (req, res) {
+  const countryName = req.params.country;
   const countryRecord = await utils.getRecordFromDB(Country, {
-    "names.name": "Singapore"
+    "names.name": "China",
   });
   const cultureRecord = await utils.getRecordFromDB(Culture, {
-    countryName: "singapore"
+    countryName: countryName,
   });
   if (!countryRecord) {
     // If not, set status 404 Not Found
     res.status(404).json({
-      error: "Country not found"
+      error: "Country not found",
     });
   } else if (!cultureRecord) {
     // If not, set status 404 Not Found
     res.status(404).json({
-      error: "Culture not found"
+      error: "Culture not found",
     });
   }
   // If exists, set status 200 OK and render the page with the object
@@ -46,5 +48,4 @@ router.get("/", async function (req, res) {
     .status(200)
     .render("country", { country: countryRecord, culture: cultureRecord });
 });
-
 module.exports = router;
