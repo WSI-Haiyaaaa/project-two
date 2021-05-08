1b9c75319b1530cbbbd675e70621a7d86e0f77dd
Haoli Yang <hyang77@hawk.iit.edu>
2021-05-08 14:24:17 -0500

    Run jsdoc and linter to fix errors and warnings

--- routes/country.js
+++ routes/country.js
@@ -1,12 +1,6 @@
 "use strict";
-const { request } = require("express");
-/** Require express framework
- * {@link https://expressjs.com/}
-    @constant
-    @type {object}
- */
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 /** Require utility functions
     @constant
     @type {object}
@@ -28,20 +22,20 @@ router.get("/:country", async function (req, res) {
   // Get country name from the request parameter
   const countryName = req.params.country;
   const countryRecord = await utils.getRecordFromDB(Country, {
-    "names.name": utils.capitalizeName(countryName),
+    "names.name": utils.capitalizeName(countryName)
   });
   const cultureRecord = await utils.getRecordFromDB(Culture, {
-    countryName: countryName,
+    countryName: countryName
   });
   if (!countryRecord) {
     // If not, set status 404 Not Found
     res.status(404).json({
-      error: "Country not found",
+      error: "Country not found"
     });
   } else if (!cultureRecord) {
     // If not, set status 404 Not Found
     res.status(404).json({
-      error: "Culture not found",
+      error: "Culture not found"
     });
   }
   // If exists, set status 200 OK and render the page with the object
